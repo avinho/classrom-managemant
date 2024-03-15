e676da04b1f2b0e1e8ef885428968a81587bbefa
Alvaro <alvaromarttinho@gmail.com>
2024-03-15 03:24:58 -0300

    adding

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -4,6 +4,8 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonCardContent,
+  IonCard,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -11,7 +13,14 @@ import {
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonCard,
+    IonCardContent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
 })
 export class Tab3Page {
   constructor() {}
