e0539bee0a21c0eb739bc31ad79cd60ed0c0abd5
Alvaro <alvaromarttinho@gmail.com>
2024-03-18 00:05:20 -0300

    adding books

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,23 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {
+  IonButton,
+  IonCard,
+  IonCardContent,
   IonContent,
   IonHeader,
-  IonTitle,
-  IonToolbar,
-  IonCardContent,
-  IonCard,
+  IonIcon,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonItem,
   IonItemDivider,
-  IonSegment,
-  IonSegmentButton,
   IonLabel,
   IonList,
-  IonIcon,
-  IonButton,
-  IonItem,
-  IonNote,
-  IonToggle,
   IonListHeader,
+  IonNote,
+  IonSegment,
+  IonSegmentButton,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToggle,
+  IonToolbar,
 } from '@ionic/angular/standalone';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-tab3',
@@ -25,6 +32,9 @@ import {
   styleUrls: ['tab3.page.scss'],
   standalone: true,
   imports: [
+    IonInfiniteScrollContent,
+    ScrollingModule,
+    IonInfiniteScroll,
     IonListHeader,
     IonToggle,
     IonNote,
@@ -42,8 +52,50 @@ import {
     IonToolbar,
     IonTitle,
     IonContent,
+    IonSelect,
+    IonSelect,
+    IonSelectOption,
+    HttpClientModule,
+    CommonModule,
   ],
 })
 export class Tab3Page {
-  constructor() {}
+  selectedBook!: Book;
+
+  books: any = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get<Book>('assets/books.json').subscribe((data) => {
+      this.books = data;
+    });
+  }
+
+  onBookChange(book: Book) {
+    this.selectedBook = book;
+    console.log(this.selectedBook);
+  }
+
+  onTopicChange(topic: Topic) {
+    topic.done = !topic.done;
+    topic.done
+      ? (topic.conclusion = new Date().toISOString())
+      : (topic.conclusion = null);
+    console.log(topic);
+  }
+}
+
+interface Book {
+  book: string;
+  lessons: Lesson[];
+}
+
+interface Lesson {
+  lesson: string;
+  topics: Topic[];
+}
+
+interface Topic {
+  topic: string;
+  done: boolean;
+  conclusion: null | string;
 }
